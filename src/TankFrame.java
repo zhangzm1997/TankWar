70e70647165e279151e8cb73cad723051ff63eff
Zemin <zhangzm1997@126.com>
2020-12-03 16:31:06 +0800

    test1

---
+++ src/TankFrame.java
@@ -0,0 +1,87 @@
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class TankFrame extends Frame {
+
+    int x = 200;
+    int y = 200;
+
+    public TankFrame() {
+        setSize(800, 600);
+        setResizable(false);
+        setTitle("Tank War");
+        setVisible(true);
+
+        this.addKeyListener(new MyKeyListener());
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.fillRect(x, y, 50, 50);
+        x+=10;
+        y+=10;
+    }
+
+
+    //处理键盘监听事件，这件事不需要
+    class MyKeyListener extends KeyAdapter {
+
+        Boolean bL = false;
+        Boolean bU = false;
+        Boolean bR = false;
+        Boolean bD = false;
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bL = true;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bL = false;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU =false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = false;
+                    break;
+                default:
+                    break;
+        }
+    }
+
+
+}}
